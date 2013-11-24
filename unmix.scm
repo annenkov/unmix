1cd341789e12561de90f552c1250a54155a56c8a
Danil Annenkov <annenkov@ib-soft.ru>
2013-11-25 00:05:20 +0900

    Changed "starting" module. Removed redundant files.

--- unmix.scm
+++ unmix.scm
@@ -1,2 +1,16 @@
-(define **unmix-path** "")
-(load (string-append **unmix-path** "xunmix.scm"))
+#lang racket/load
+;; This file is used to run Unmix under Racket. 
+(require "x-match.scm"
+         "xmenu.scm" )
+
+(for-each
+ (lambda (x) (display x) (newline))
+ '(""
+   "UNMIX 3.0 (December 1993) for MacGambit Scheme"
+   "   Sergei A.Romanenko, Keldysh Institute of Applied Mathematics,"
+   "   Russian Academy of Sciences, Miusskaya Sq.4, 125047, Moscow, Russia"
+   "")
+ )
+
+(display "Loading Unmix")(newline)
+(unmix)
