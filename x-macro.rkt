4f7f30d158b6738fa8b5b1c1e97d6b67bf716f74
Danil Annenkov <annenkov@ib-soft.ru>
2013-12-22 01:17:56 +0900

    sex->rkt compiler fix. Added expanders.

--- x-macro.rkt
+++ x-macro.rkt
@@ -294,4 +294,26 @@
     (newline) (display "--- Done ---") (newline)
     ))
 
+; installing default expanders
+
+(ux:install-macro
+  'rcall
+  (lambda (x)
+    (cond ((syntax-match? '(rcall) '(rcall exp) x) (cadr x))
+          (else (error "rcall: invalid syntax " x)))))
+
+(ux:install-macro
+  'generalize
+  (lambda (x)
+    (cond ((syntax-match? '(generalize) '(generalize exp) x) (cadr x))
+          (else (error "generalize: invalid syntax " x)))))
+
+(ux:install-macro
+  'when
+  (lambda (x)
+    (cond ((syntax-match? '(when) '(when pred exp1 exp2 ...) x)
+           `(if ,(cadr x) (begin unquote (cddr x))))
+          (else (error "when: invalid syntax " x)))))
+
+
 (provide (all-defined-out))
